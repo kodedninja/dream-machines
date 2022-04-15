dd3f8233a9b0030aafb5183f279797d60dfab11a
hunor <hunor.karaman@geogebra.org>
2022-04-15 10:43:40 +0200

    *

--- app.js
+++ app.js
@@ -8,9 +8,27 @@ var content = fs.readFileSync(path.join(__dirname, 'text.md'), 'utf8')
 
 var app = choo()
 
+app.use(require('choo-meta')())
+
 app.route('/', view)
 
 function view (store, emit) {
+  emit('meta', {
+    'title': 'Inside Dream Machines',
+    'description': 'Inside Dream Machines. A phenomenology of digital ghosts. By Hunor Karamán',
+    'keywords': 'Dream machines, digital, new worlds, simulosis, cyborg, phenomenology',
+    'og:title': 'Inside Dream Machines',
+    'og:site_name': 'hex22.org',
+    'og:url': 'https://hex22.org/inside-dream-machines/',
+    'og:description': 'A phenomenology of digital ghosts.',
+    'og:type': 'website',
+    'og:locale': 'en_US',
+    'twitter:card': 'summary',
+    'twitter:title': 'Inside Dream Machines',
+    'twitter:description': 'A phenomenology of digital ghosts.',
+    'twitter:url': 'https://hex22.org/inside-dream-machines/'
+  })
+  
   return html`
     <body>
       <div class="p-a o-0_8 w-100" style="z-index:-1">
--- index.js
+++ index.js
@@ -2,9 +2,34 @@ var fs = require('fs')
 var path = require('path')
 var app = require('./app')
 var ncp = require('ncp').ncp
+var dedent = require('dedent')
 
-var html = fs.readFileSync(path.join(__dirname, 'template.html'), 'utf8')
-html = html.replace('%%BODY%%', app.toString('/'))
+var TEMPLATE = dedent`
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="/index.css" rel="stylesheet">
+
+    %%HEAD%%
+  </head>
+  %%BODY%%
+</html>
+`
+
+var state = {}
+var body = app.toString('/', state)
+
+var head = dedent`
+  <title>${state.title}</title>
+  ${Object.keys(state.meta).map(function (key) {
+    var attribute = key.substr(0, 3) === 'og:' ? 'property' : 'name'
+    return '<meta ' + attribute + '="' + key + '" content="' + state.meta[key] + '" >'
+  }).join('\n')}
+`
+var html = TEMPLATE.replace('%%BODY%%', body).replace('%%HEAD%%', head)
 
 if (!fs.existsSync('public')) {
   fs.mkdirSync('public')
