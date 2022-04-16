167f653d6f4226e74201cdada474516271dfff8f
hunor <hunor.karaman@geogebra.org>
2022-04-16 12:09:32 +0200

    build

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var fs = require('fs')
 var path = require('path')
-var app = require('./app')
 var ncp = require('ncp').ncp
 var dedent = require('dedent')
 var browserify = require('browserify')
 var nanohtml = require('nanohtml')
+var concat = require('concat-stream')
+var crypto = require('crypto')
+
+var app = require('./app')
 
 var TEMPLATE = dedent`
 <!DOCTYPE HTML>
@@ -14,18 +17,13 @@ var TEMPLATE = dedent`
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="preload" href="/assets/index.css" as="style">
-    <link href="/assets/index.css" rel="stylesheet">
-
     %%HEAD%%
   </head>
   %%BODY%%
 </html>
 `
 
-var SCRIPT = '<script src="/assets/index.js" defer></script>'
-
-function bundleHTML() {
+function bundleHTML(bundleState) {
   var state = {}
   var body = app.toString('/', state)
 
@@ -35,37 +33,85 @@ function bundleHTML() {
       var attribute = key.substr(0, 3) === 'og:' ? 'property' : 'name'
       return '<meta ' + attribute + '="' + key + '" content="' + state.meta[key] + '" >'
     }).join('\n')}
+ 
+    <link rel="preload" href="${bundleState.stylePath}" as="style">
+    <link href="${bundleState.stylePath}" rel="stylesheet">
 
     <script>window.initialState = ${JSON.stringify(state)}</script>
   `
+  var script = `<script src="${bundleState.scriptPath}" defer></script>`
+  
   var html = TEMPLATE.replace('%%BODY%%', body).replace('%%HEAD%%', head)
-  html = html.replace('</body>', `${SCRIPT}</body>`)
+  html = html.replace('</body>', `${script}</body>`)
 
   fs.writeFileSync(path.join('public', 'index.html'), html)
 }
 
 function bundleScripts() {
-  var b = browserify()
+  return new Promise(function (resolve) {
+    var sha512 = crypto.createHash('sha512')
+    
+    var b = browserify()
 
-  b.add('./app.js')
+    b.add('./app.js')
 
-  b.transform('brfs')
-  b.transform(nanohtml)
-  b.plugin('tinyify')
+    b.transform('brfs')
+    b.transform(nanohtml)
+    b.plugin('tinyify')
 
-  b.bundle().pipe(fs.createWriteStream('public/assets/index.js'))
+    var bundleStream = b.bundle()
+    bundleStream.on('data', function (chunk) {
+      sha512.update(chunk)
+    })
+
+    bundleStream.pipe(concat({ type: 'buffer' }, function (bundle) {
+      var hash = sha512.digest('buffer').toString('hex').slice(0, 16)
+      var scriptPath = `/assets/index.${hash}.js`
+      
+      var ws = fs.createWriteStream(path.join(`public`, scriptPath))
+      ws.write(bundle)
+      ws.end()
+
+      resolve(scriptPath)
+    }))
+  })
 }
 
-function copyAssets() {
-  var assetsPath = path.join(__dirname, 'assets')
-  if (fs.existsSync(assetsPath)) {
-    ncp(assetsPath, 'public/assets', function (err) {
-      if (err) throw err
-    })
-  }
+function bundleStyles () {
+  return new Promise(function (resolve) {
+    var sha512 = crypto.createHash('sha512')
 
-  ncp(path.join(__dirname, 'index.css'), 'public/assets/index.css', function (err) {
-    if (err) throw err
+    var rs = fs.createReadStream(path.join(__dirname, 'index.css'))
+    rs.on('data', function (chunk) {
+      sha512.update(chunk)
+    })
+
+    rs.pipe(concat({ type: 'buffer' }, function (bundle) {
+      var hash = sha512.digest('buffer').toString('hex').slice(0, 16)
+      var stylePath = `/assets/index.${hash}.css`
+
+      var ws = fs.createWriteStream(path.join(`public`, stylePath))
+      ws.write(bundle)
+      ws.end()
+
+      resolve(stylePath)
+    }))
+  })
+}
+
+function copyAssets (state) {
+  return new Promise(function (resolve, reject) {
+    var assetsPath = path.join(__dirname, 'assets')
+    if (fs.existsSync(assetsPath)) {
+      ncp(assetsPath, 'public/assets', function (err) {
+        if (err) {
+          reject(err)
+          return
+        }
+
+        resolve()
+      })
+    }
   })
 }
 
@@ -77,7 +123,16 @@ if (!fs.existsSync('public/assets')) {
     fs.mkdirSync('public/assets')
 }
 
-bundleScripts()
-copyAssets()
-bundleHTML()
+Promise.all([
+  bundleScripts(),
+  bundleStyles(),
+  copyAssets()
+]).then(function (paths) {
+  console.log(paths[0])
+  console.log(paths[1])
+  bundleHTML({
+    scriptPath: paths[0],
+    stylePath: paths[1]
+  })
+})
 
